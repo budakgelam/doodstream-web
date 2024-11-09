cab57691b3312ac50ccef10b93691ede48de7fda
budakgelam <131536307+budakgelam@users.noreply.github.com>
2024-11-09 21:25:20 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,32 +2,19 @@ import type { MetadataRoute } from 'next'
 
 const serverUrl = 'http://localhost:3000'
 
-export default async function sitemap(): MetadataRoute.Sitemap {
-  const { docs: pages } = await fetch(`${serverUrl}/api/pages?limit=0`).then((res) => res.json())
-
-  const { docs: posts } = await fetch(`${serverUrl}/api/posts?limit=0`).then((res) =>
-    res.json(),
-  )
-
-  const sitemap: MetadataRoute.Sitemap = []
-
-  for (const page of pages) {
-    sitemap.push({
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: '${serverUrl}',
+      lastModified: new Date(),
       changeFrequency: 'daily',
-      lastModified: page.updatedAt,
       priority: 1,
-      url: `${serverUrl}/${page.slug === 'home' ? '' : page.slug}`,
-    })
-  }
-
-  for (const post of posts) {
-    sitemap.push({
+    },
+    {
+      url: '${serverUrl}/v/${video.file_code}',
+      lastModified: new Date(),
       changeFrequency: 'daily',
-      lastModified: post.updatedAt,
-      priority: 1,
-      url: `${serverUrl}/v/${post.slug}`,
-    })
-  }
-
-  return sitemap
+      priority: 0.8,
+    }, 
+  ]
 }
