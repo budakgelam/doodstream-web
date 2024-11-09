7307fdeeb44f2504ea088cdffa9a173805065df4
budakgelam <131536307+budakgelam@users.noreply.github.com>
2024-11-09 20:41:36 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,12 +3,12 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: '/',
+      url: '${BASE_URL}',
       lastModified: new Date(),
       priority: 1,
     },
     {
-      url: '/v/${video.file_code',
+      url: '${BASE_URL}/v/${video.file_code',
       lastModified: new Date(),
       priority: 0.8,
     },
