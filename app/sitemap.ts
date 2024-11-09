ba9b2ddf3c459481e0f57e38a56dc7c5ef953b1d
budakgelam <131536307+budakgelam@users.noreply.github.com>
2024-11-09 22:23:10 +0700

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+import { connectToDatabase }
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: '${process.env.NEXT_WEBSITE_URL}',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: '${process.env.NEXT_WEBSITE_URL}/v/${item.slug}',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: '${process.env.NEXT_WEBSITE_URL}/c/${item.slug}',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
